a846c87809ca9c36a3194edfb641f30f0cbbd29a
Fred Malm <55448542+FredrikMalmborg@users.noreply.github.com>
2019-10-28 11:36:56 +0100

    custom scrollbar in chrome: wonder if it works in firefox & other browsers... nvm it's pointless

--- script.js
+++ script.js
@@ -1,22 +1,62 @@
 let input = document.querySelector("input"),
-    display = document.querySelector(".terminal")
+    display = document.querySelector(".terminal"),
+    inventory = ["pen", "jar-of-dirt", "paste"]
+
+// var object = {
+//     name: "pen",
+//     count: 1,
+//     inInventory: false
+// }
 
 
-document.querySelector("button").onclick = function () {
-    display.innerText = input.value
-}
 
-
-
-const log = document.getElementById('log');
 
 document.addEventListener('keypress', logKey);
-
 function logKey(e) {
-    console.log(` ${e.code}`)
     if (` ${e.code}` === " Enter") {
-        console.log("brug")
-        display.innerText = input.value
+
+        writeToTerminal()
+        
+        input.placeholder = "do something"
+
+        if(input.value === "use"){
+            input.placeholder = "Use what item?"
+            use();
+        }else if(input.value === "help"){
+
+        }
+
         input.value = ""
     }
 }
+
+function refreshInventory() {
+    let inventoryList = document.querySelectorAll(".inventory ul li")
+
+    for (let i = 0; targetedItem = inventoryList[i]; i++) {
+        targetedItem.parentNode.removeChild(targetedItem)
+    }
+
+    inventory.forEach(item => {
+        let listNode = document.createElement('li'),
+            textNode = document.createTextNode(item)
+        listNode.appendChild(textNode)
+        document.querySelector("ul").appendChild(listNode)
+    })
+}
+function use() {
+    console.log("jonger")
+}
+
+function writeToTerminal() {
+    let listNode = document.createElement('p'),
+    textNode = document.createTextNode(input.value)
+
+    listNode.appendChild(textNode)
+    document.querySelector(".terminal").appendChild(listNode)
+}
+
+
+
+
+refreshInventory()
